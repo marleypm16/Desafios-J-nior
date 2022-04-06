e90ea26affc3aa30afeb3dd5385adae982193392
marleypm16 <yarlempm6@gmail.com>
2022-04-06 08:34:36 -0300

    Desafios FrontEndMentor nivel Junior

---
+++ fylo data landing page/script.js
@@ -0,0 +1,35 @@
+const email=document.getElementById('email')
+const getaccess=document.getElementById('get')
+
+getaccess.addEventListener("submit", (e) =>{
+    e.preventDefault()
+    checkInput()
+})
+
+function checkInput(){
+
+    const emailvalue=email.value
+
+    if(emailvalue == ''){
+
+        SetError(email,'Please enter a email adress')
+    }
+    else{
+        SetSucesso(email)
+    }
+
+}
+
+function SetError(input,mensagem){
+
+   const button=input.parentElement;
+   const small= button.querySelector('small')
+   small.innerText=mensagem
+   button.className='input erro'
+}
+
+function SetSucesso(input){
+
+    const button= input.parentElement;
+    button.className='input sucesso'
+}
