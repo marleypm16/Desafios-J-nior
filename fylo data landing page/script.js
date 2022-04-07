071000d257e10eb0402fae929d7a02783b44573d
marleypm16 <yarlempm6@gmail.com>
2022-04-07 10:12:35 -0300

    desafio + testes

--- fylo data landing page/script.js
+++ fylo data landing page/script.js
@@ -1,5 +1,7 @@
 const email=document.getElementById('email')
 const getaccess=document.getElementById('get')
+const signin= document.getElementById('signin')
+const signinconteiner=document.querySelector('.signin-conteiner')
 
 
 getaccess.addEventListener("submit", (e) =>{
@@ -37,4 +39,10 @@ function SetSucesso(input){
 
     const button= input.parentElement;
     button.className='input sucesso'
+}
+
+signin.onclick= function(){
+
+    signinconteiner.classList.toggle("active")
+
 }
--- sidebar/script.js
+++ sidebar/script.js
@@ -4,11 +4,11 @@ let sidebar=document.querySelector(".sidebar");
 let searchBtn=document.querySelector(".fa-magnifying-glass")
 
 btn.onclick= function(){
-
     sidebar.classList.toggle("active")
 }
 
 searchBtn.onclick= function(){
 
-    sidebar.classList.toggle("active")
+    sidebar.classList.toggle("active") //permite que a classe volte ao que era antes.
+    //entao ao clicar a class sidebar vira sidebar active e ao clicar novamente volta a ser sidebar
 }
