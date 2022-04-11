529e29cb81d314e5f9e85b938025deb4326f7ea8
marleypm16 <yarlempm6@gmail.com>
2022-04-10 22:05:08 -0300

    sign up

---
+++ sidebar/signin.js
@@ -0,0 +1,62 @@
+const email = document.getElementById('email')
+const user = document.getElementById('username')
+const senha= document.getElementById('senha')
+const confirma= document.getElementById('confirm')
+const button= document.getElementById('verificar')
+
+button.onclick = (e) =>{
+    e.preventDefault()
+
+    checkInput()
+}
+
+function checkInput(){
+    const emailvalue= email.value
+    const uservalue = user.value
+    const senhavalue = senha.value
+    const confirmvalue=confirma.value
+
+    if(emailvalue == ''){
+        setErro(email, 'email cannot be blank')
+    }
+    else{
+        setSucesso(email)
+    }
+    if(uservalue == ''){
+        setErro(user, 'Username cannot be blank')
+    }
+    else{
+        setSucesso(user)
+    }
+    if(senhavalue == ''){
+        setErro(senha, 'Password cannot be blank')
+    }
+    else if(senhavalue.length < 7){
+        setErro(senha,'Password must be at least 7 characters')
+    }
+    else{
+        setSucesso(senha)
+    }
+    if(confirmvalue == ''){
+        setErro(confirma, 'Password Confirmation cannot be blank')
+    }
+    else if(confirmvalue != senhavalue){
+        setErro(confirma,'Passwords do not match')
+    }
+    else{
+        setSucesso(confirma)
+    }
+   
+}
+
+function setErro(input,mensagem){
+    const conteiner = input.parentElement;
+    const small= conteiner.querySelector('small')
+    small.innerText=mensagem
+    conteiner.className='conteiner erro'
+}
+
+function setSucesso(input){
+    const conteiner = input.parentElement;
+    conteiner.className='conteiner sucesso'
+}
